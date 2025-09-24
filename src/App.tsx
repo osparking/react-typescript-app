9225f25555bf407d4c047a5b5bf69be2804dcd05
BumTiger <jbpark03@gmail.com>
2025-09-24 11:41:06 +0900

    arrow function syntax for calculate

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function calculate<T>(param: T) : T {
+const calculate = <T,>(param: T) : T => {
   return param;
 }
 
