9773606d007d45cdd738b452cfdaefd466aeb867
BumTiger <jbpark03@gmail.com>
2025-09-25 06:19:57 +0900

    item entry form submit event handler

--- src/component/ItemEntryForm.tsx
+++ src/component/ItemEntryForm.tsx
@@ -1,8 +1,13 @@
 import React, { JSX } from 'react'
 
+function handleItemEntry(e: React.FormEvent) {
+  e.preventDefault();
+  console.log("항목 입력됨");
+}
+
 const ItemEntryForm = ():JSX.Element => {
   return (
-    <form>
+    <form onSubmit={handleItemEntry}>
       <input type="text" placeholder='구매 항목 입력'/>
       <button type="submit">항목 저장</button>
     </form>
