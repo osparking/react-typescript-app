1df77b69096e9e4a1e751331cb8d768ed984860a
BumTiger <jbpark03@gmail.com>
2025-09-24 19:24:29 +0900

    destructure props into {items}

--- src/component/ShoppingList.tsx
+++ src/component/ShoppingList.tsx
@@ -10,12 +10,12 @@ interface ListParam {
   items: ShopItem[];
 }
 
-const ShoppingList = (props : ListParam): JSX.Element => {
+const ShoppingList = ({items} : ListParam): JSX.Element => {
   return (
     <div>
       <h1>쇼핑 리스트</h1>
       <ul>
-        {props.items.map((item) =>
+        {items.map((item) =>
           <li key={item.id}>{item.product} - {item.quantity}</li>
         )}
       </ul>
