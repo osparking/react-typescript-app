af6a6ec8dd3024b74dba0ca84aa1027ce71feb5c
BumTiger <jbpark03@gmail.com>
2025-09-24 20:21:34 +0900

    useState used for items array - phase 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 import ShoppingList from './component/ShoppingList';
 
 function App() {
-  const items = [{
-    id: 1,
-    product: "수수",
-    quantity: 500
-  }, {
-    id: 2,
-    product: "발가락 양말",
-    quantity: 2
-  }];
+  const [items, setItems] = useState([]);
+  // const items = [{
+  //   id: 1,
+  //   product: "수수",
+  //   quantity: 500
+  // }, {
+  //   id: 2,
+  //   product: "발가락 양말",
+  //   quantity: 2
+  // }];
 
   return (
     <div>
