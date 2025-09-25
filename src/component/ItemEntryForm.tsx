3a9d11a9548234bc9d01fde6fb3e4c15e5288e2d
BumTiger <jbpark03@gmail.com>
2025-09-25 09:13:48 +0900

    adds useRef const to refer input element

--- src/component/ItemEntryForm.tsx
+++ src/component/ItemEntryForm.tsx
@@ -1,15 +1,16 @@
-import React, { JSX } from 'react'
+import React, { JSX, useRef } from 'react'
 
 const ItemEntryForm = ():JSX.Element => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   function handleItemEntry(e: React.FormEvent) {
     e.preventDefault();
     console.log("항목 입력됨");
   }
-  
+
   return (
     <form onSubmit={handleItemEntry}>
-      <input type="text" placeholder='구매 항목 입력'/>
+      <input type="text" placeholder='구매 항목 입력' ref={inputRef} />
       <button type="submit">항목 저장</button>
     </form>
   )
